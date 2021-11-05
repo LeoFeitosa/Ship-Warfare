ce183624adda789e780de58b421eadea22fd5219
LeoFeitosa <>
2021-11-05 15:31:18 -0300

    adjust parallax desert

--- Assets/Runtime/Scripts/Backgrounds/ParallaxController.cs
+++ Assets/Runtime/Scripts/Backgrounds/ParallaxController.cs
@@ -4,11 +4,71 @@ using UnityEngine;
 
 public class ParallaxController : MonoBehaviour
 {
-    [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] float speed = 1f;
+    [SerializeField] float offset = 1f;
+    [SerializeField] bool randomFlipX = false;
+    SpriteRenderer render;
+    float imageSizeY;
+    bool checkInstantiate = false;
+    GameObject instanceBackground;
+
+    void Start()
+    {
+        render = GetComponentInChildren<SpriteRenderer>();
+        imageSizeY = render.size.y;
+    }
 
     void FixedUpdate()
     {
-        meshRenderer.material.mainTextureOffset += new Vector2(0, speed * Time.deltaTime);
+        MoveBackground();
+        InstantiateBackground();
+        DestroyInstanceBackground();
+    }
+
+    void MoveBackground()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    void InstantiateBackground()
+    {
+        if (!checkInstantiate)
+        {
+            if (transform.position.y <= imageSizeY)
+            {
+                checkInstantiate = true;
+                instanceBackground = Instantiate(this.gameObject);
+                instanceBackground.transform.position = new Vector3(transform.position.x, transform.position.y + (imageSizeY * 2) + offset, transform.position.z);
+
+                if (randomFlipX)
+                {
+                    if (RandBool())
+                    {
+                        SpriteRenderer instanceRenderer = instanceBackground.GetComponentInChildren<SpriteRenderer>();
+                        if (instanceRenderer != null)
+                        {
+                            instanceRenderer.flipX = !instanceRenderer.flipX;
+                        }
+                    }
+                }
+
+                Debug.Log("instanciar");
+            }
+        }
+    }
+
+    void DestroyInstanceBackground()
+    {
+        if (transform.position.y <= (imageSizeY * 2) * -1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    bool RandBool()
+    {
+        float rand = Random.Range(0, 100);
+        Debug.Log(rand);
+        return (rand < 50) ? true : false;
     }
 }
