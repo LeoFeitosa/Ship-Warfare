2b104e99e343b69d5aa88d7fdaa9bc0451e684d5
LeoFeitosa <>
2021-11-09 13:55:19 -0300

    adjust power up

--- Assets/Runtime/Scripts/PowerUps/PowerUpsController.cs
+++ Assets/Runtime/Scripts/PowerUps/PowerUpsController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PowerUpsController : MonoBehaviour
 {
     [SerializeField] float duration = 2;
+    [SerializeField] float sizeAfterPickingUp = 0.832f;
+    Transform targetPositionInUI;
+    [SerializeField] float speedTargetPosition = 1.5f;
     public static float PowerUpDuration { get; private set; }
+    bool movePowerUp = false;
     bool timerIsRunning = false, initTimerIsRunning = false;
     ShotsTypesController shotsTypes;
+    Color colorPowerUp;
+
+    void Start()
+    {
+        colorPowerUp = GetComponent<SpriteRenderer>().color;
+        GameObject backgroundPowerUp = GameObject.FindWithTag("BackgroundPowerUp");
+        if (backgroundPowerUp != null)
+        {
+            targetPositionInUI = backgroundPowerUp.transform;
+        }
+    }
 
     void FixedUpdate()
     {
         InitTimerPowerUp();
         TimerPowerUp();
+        MovePowerUp();
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -78,9 +95,37 @@ public class PowerUpsController : MonoBehaviour
 
     void HideObject()
     {
-        gameObject.GetComponent<CircleCollider2D>().enabled = false;
-        gameObject.GetComponent<SpriteRenderer>().enabled = false;
-        gameObject.GetComponent<Animator>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        transform.localScale = new Vector3(sizeAfterPickingUp, sizeAfterPickingUp, transform.localScale.z);
+
+        GameObject[] powerUpTripleInScene = GameObject.FindGameObjectsWithTag("PowerUpShotTriple");
+        GameObject[] powerUpDoubleInScene = GameObject.FindGameObjectsWithTag("PowerUpShotDouble");
+        GameObject[] powerUpsInScene = powerUpTripleInScene.Concat(powerUpDoubleInScene).ToArray();
+
+        foreach (var item in powerUpsInScene)
+        {
+            SpriteRenderer render = item.gameObject.GetComponent<SpriteRenderer>();
+            CircleCollider2D collider = item.gameObject.GetComponent<CircleCollider2D>();
+            if (render != null && collider.enabled == false)
+            {
+                render.sortingLayerID = SortingLayer.NameToID("UI");
+                render.sortingOrder = 1;
+            }
+        }
+
+        GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("UI");
+        GetComponent<SpriteRenderer>().sortingOrder = 10;
+
+        movePowerUp = true;
+    }
+
+    void MovePowerUp()
+    {
+        if (movePowerUp && targetPositionInUI != null)
+        {
+            float step = speedTargetPosition * Time.fixedDeltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPositionInUI.position, step);
+        }
     }
 
     void TimerPowerUp()
@@ -94,6 +139,12 @@ public class PowerUpsController : MonoBehaviour
                 PowerUpDuration = 0;
                 PowerUpNormal();
                 Destroy(gameObject);
+            }
+
+            if (colorPowerUp != null)
+            {
+                Debug.Log("addddddd");
+                colorPowerUp.a = PowerUpDuration;
             }
         }
     }
