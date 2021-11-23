1e0dc7538827180d7f33452a9d5f51d404f712d4
LeoFeitosa <>
2021-11-23 12:27:37 -0300

    kill with enemy with shot

--- Assets/Runtime/Scripts/Enemies/EnemyController.cs
+++ Assets/Runtime/Scripts/Enemies/EnemyController.cs
@@ -7,15 +7,23 @@ public class EnemyController : MonoBehaviour
 {
     EnemyAnimationController enemyAnimation;
     PlayerController player;
+    BoxCollider2D colliderEnemy;
 
     void Start()
     {
         enemyAnimation = GetComponent<EnemyAnimationController>();
+        colliderEnemy = GetComponent<BoxCollider2D>();
         player = FindObjectOfType<PlayerController>();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.CompareTag("PlayerShot"))
+        {
+            Die();
+            Destroy(col.gameObject);
+        }
+
         if (col.CompareTag("Player") && (player != null && player.PlayerColliderWithEnemy))
         {
             Die();
@@ -24,6 +32,7 @@ public class EnemyController : MonoBehaviour
 
     void Die()
     {
+        colliderEnemy.enabled = false;
         enemyAnimation.IsDead = true;
     }
 
