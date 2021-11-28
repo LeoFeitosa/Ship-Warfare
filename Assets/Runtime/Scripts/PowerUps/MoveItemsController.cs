99d7fb61306d657861f42ce715622136fadb2f63
LeoFeitosa <>
2021-11-28 17:11:44 -0300

    rotate powerups

--- Assets/Runtime/Scripts/PowerUps/MoveItemsController.cs
+++ Assets/Runtime/Scripts/PowerUps/MoveItemsController.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveItemsController : MonoBehaviour
 {
     [SerializeField] float speedMove;
+    [SerializeField] float speedRoration;
     [SerializeField] float limitMoveX;
     [SerializeField] float limitMoveY;
     Vector3 moveDirections = Vector3.zero;
     bool stopMove = true;
-
+    [SerializeField] float angleZ = 45;
 
     void Start()
     {
         moveDirections = RandomBool() ? DirectionPositive() : DirectionNegative();
         stopMove = false;
+
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.x,
+            transform.rotation.y,
+            angleZ);
     }
 
     void LateUpdate()
     {
         Move();
+        PowerUpRotation();
     }
 
     void Move()
@@ -60,6 +68,20 @@ public class MoveItemsController : MonoBehaviour
         }
 
         return false;
+    }
+
+    void PowerUpRotation()
+    {
+        //corrigir rotate
+        RotateY(-angleZ);
+    }
+
+    void RotateY(float angle)
+    {
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            Mathf.LerpAngle(transform.eulerAngles.z, angle, Time.deltaTime * speedRoration));
     }
 
     void OnTriggerEnter2D(Collider2D col)
