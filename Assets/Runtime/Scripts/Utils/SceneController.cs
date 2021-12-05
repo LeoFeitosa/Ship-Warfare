960ef6ebe2208d88e9103c08a71f464758a2d8e6
LeoFeitosa <>
2021-12-05 12:01:37 -0300

    normalize audio

--- Assets/Runtime/Scripts/Utils/SceneController.cs
+++ Assets/Runtime/Scripts/Utils/SceneController.cs
@@ -31,7 +31,7 @@ public class SceneController : MonoBehaviour
 
     IEnumerator LoadAsyncScene(string name)
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(name);
 
         while (!asyncLoad.isDone)
